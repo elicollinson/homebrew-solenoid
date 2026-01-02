f108d8d3e28797376f54b6cdca36c1864cd06f59
Eli Collinson <elicollinson1@gmail.com>
2026-01-02 17:03:28 -0500

    version bump to 1.1.0

--- solenoid.rb
+++ solenoid.rb
@@ -1,9 +1,9 @@
 class Solenoid < Formula
   desc "A localized AI agent for the terminal"
   homepage "https://github.com/elicollinson/Solenoid"
-  url "https://github.com/elicollinson/Solenoid/releases/download/v1.0.0-beta/submission.zip"
-  sha256 "98c4a924b9e711b65416a9cd310f9a9f77efc81bb88981495fc3168239c9b7ff"
-  version "1.0.0"
+  url "https://github.com/elicollinson/Solenoid/archive/refs/tags/v1.1.0-beta.tar.gz"
+  sha256 "ebbe84191eaf7f5fc3bef888a169f8527493ac40a02379156ae4033a39870355"
+  version "1.1.0"
 
   def install
     # This moves the binary into the user's path hidden folder
